db02aba5e275a2df5f94da9505e150f33fcfb5bf
Kristian Andersen <hello@kristian.co>
2017-02-13 12:40:33 +0100

    Stop spinning ladda buttons on load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 var $ = require('jquery');
+var Ladda = require('ladda');
 var Retina = require('retina.js').Retina;
 
 $(document).ready(function() {
+  Ladda.stopAll();
   Retina.init(window);
 });
